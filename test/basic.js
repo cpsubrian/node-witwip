5167f8b7a27886e678b1649d27b2cb8c4c3d9dfd
Brian Link <cpsubrian@gmail.com>
2012-08-28 17:07:24 -0400

    Remove node_modules dir from tests.

--- test/basic.js
+++ test/basic.js
@@ -1,42 +1,43 @@
 var witwip = require('../'),
+    path = require('path'),
     assert = require('assert');
 
 describe('basic functionality', function() {
-  var path, info;
+  var pkgPath, info;
 
   it('can find package.json of a base module', function() {
-    path = witwip(module);
-    info = require(path);
+    pkgPath = witwip(module);
+    info = require(pkgPath);
     assert.equal(info.name, 'basic');
   });
 
   it('can find package.json of local module', function() {
-    path = witwip(module, './local/chief');
-    info = require(path);
+    pkgPath = witwip(module, './local/chief');
+    info = require(pkgPath);
     assert.equal(info.name, 'chief');
   });
 
   it('can find package.json recursing out of a sub-folder', function() {
-    path = witwip(__dirname + '/local/chief/subfolder/deeper');
-    info = require(path);
+    pkgPath = witwip(__dirname + '/local/chief/subfolder/deeper');
+    info = require(pkgPath);
     assert.equal(info.name, 'chief');
   });
 
   it('can find package.json of a module', function() {
-    path = witwip(module, 'carmen');
-    info = require(path);
-    assert.equal(info.name, 'carmen');
+    pkgPath = witwip(module, 'debug');
+    info = require(pkgPath);
+    assert.equal(info.name, 'debug');
   });
 
   it('throws when a match cannot be found', function() {
     assert.throws(function(){
-      path = witwip(__dirname, './local/double-trouble');
+      pkgPath = witwip(__dirname, './local/double-trouble');
     });
   });
 
-  it('can return the directory instead of the filepath', function() {
-    path = witwip.dir(module, 'carmen');
-    assert.equal(path, __dirname + '/node_modules/carmen');
+  it('can return the directory instead of the filepkgPath', function() {
+    pkgPath = witwip.dir(module, 'debug');
+    assert.equal(pkgPath, path.resolve(__dirname, '..', 'node_modules/debug'));
   });
 
 });
