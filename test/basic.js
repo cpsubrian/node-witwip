0fe95ed015f51c0ca69178162eb8e2eedfe35786
Brian Link <cpsubrian@gmail.com>
2012-09-12 16:28:23 -0400

    Changes to an async API.  Support package comments in index.js.

--- test/basic.js
+++ test/basic.js
@@ -3,41 +3,68 @@ var witwip = require('../'),
     assert = require('assert');
 
 describe('basic functionality', function() {
-  var pkgPath, info;
 
-  it('can find package.json of a base module', function() {
-    pkgPath = witwip(module);
-    info = require(pkgPath);
-    assert.equal(info.name, 'basic');
-  });
-
-  it('can find package.json of local module', function() {
-    pkgPath = witwip(module, './local/chief');
-    info = require(pkgPath);
-    assert.equal(info.name, 'chief');
-  });
-
-  it('can find package.json recursing out of a sub-folder', function() {
-    pkgPath = witwip(__dirname + '/local/chief/subfolder/deeper');
-    info = require(pkgPath);
-    assert.equal(info.name, 'chief');
-  });
-
-  it('can find package.json of a module', function() {
-    pkgPath = witwip(module, 'debug');
-    info = require(pkgPath);
-    assert.equal(info.name, 'debug');
-  });
-
-  it('throws when a match cannot be found', function() {
-    assert.throws(function(){
-      pkgPath = witwip(__dirname, './local/double-trouble');
+  it('can find package.json of the current module', function(done) {
+    witwip(function(err, pkgPath, info) {
+      assert.ifError(err);
+      assert.equal(info.name, 'basic');
+      done();
     });
   });
 
-  it('can return the directory instead of the filepkgPath', function() {
-    pkgPath = witwip.dir(module, 'debug');
-    assert.equal(pkgPath, path.resolve(__dirname, '..', 'node_modules/debug'));
+  it('can find package.json of a base module', function(done) {
+    witwip(module, function(err, pkgPath, info) {
+      assert.ifError(err);
+      assert.equal(info.name, 'basic');
+      done();
+    });
+  });
+
+  it('can find package.json of local module', function(done) {
+    witwip(module, './local/chief', function(err, pkgPath, info) {
+      assert.ifError(err);
+      assert.equal(info.name, 'chief');
+      done();
+    });
+  });
+
+  it('can find a module that uses a package comment', function(done) {
+    witwip(module, './local/carmen', function(err, pkgPath, info) {
+      assert.ifError(err);
+      assert.equal(info.name, 'carmen');
+      done();
+    });
+  });
+
+  it('can find package.json recursing out of a sub-folder', function(done) {
+    witwip(__dirname + '/local/chief/subfolder/deeper', function(err, pkgPath, info) {
+      assert.ifError(err);
+      assert.equal(info.name, 'chief');
+      done();
+    });
+  });
+
+  it('can find package.json of a module', function(done) {
+    witwip(module, 'debug', function(err, pkgPath, info) {
+      assert.ifError(err);
+      assert.equal(info.name, 'debug');
+      done();
+    });
+  });
+
+  it('errors when a match cannot be found', function(done) {
+    witwip(__dirname, './local/nothere', function(err, pkgPath, info) {
+      assert.equal(err.code, 'ENOENT');
+      done();
+    });
+  });
+
+  it('can return the directory instead of the file path', function(done) {
+    witwip.dir(module, 'debug', function(err, pkgDir, info) {
+      assert.ifError(err);
+      assert.equal(pkgDir, path.resolve(__dirname, '..', 'node_modules/debug'));
+      done();
+    });
   });
 
 });
--- witwip.js
+++ witwip.js
@@ -1,19 +1,36 @@
 var path = require('path'),
     fs = require('fs'),
-    existsSync = fs.existsSync ? fs.existsSync : path.existsSync,
-    modulePathCache = {},
-    debug = require('debug')('witwip');
+    exists = fs.exists || path.exists,
+    existsSync = fs.existsSync || path.existsSync,
+    debug = require('debug')('witwip'),
+    readJSON = require('read-package-json'),
+    modulePathCache = {};
+
+readJSON.log = {
+  info: function() {},
+  verbose: function() {},
+  warn: function() {}
+};
 
 module.exports = witwip;
 
-function witwip(base, modulePath) {
-  var originalBase, newPath;
+function witwip(base, modulePath, callback) {
+  // If only a callback is passed, then find the package.json of the caller.
+  if (typeof base === 'function') {
+    callback = base;
+    modulePath = null;
+    base = module.parent;
+  }
 
   // Allow base to be a module object (such as `module.parent`).
   if (base.filename) {
     base = path.dirname(base.filename);
   }
-  originalBase = base;
+
+  if (typeof modulePath === 'function') {
+    callback = modulePath;
+    modulePath = null;
+  }
 
   // If no modulePath is passed, check the base directory.
   modulePath = modulePath || '.';
@@ -24,54 +41,76 @@ function witwip(base, modulePath) {
   }
   var cache = modulePathCache[base];
   if (cache.hasOwnProperty(modulePath)) {
-      debug('Found `' + modulePath + '` in `' + base + '` cache');
-      return cache[modulePath];
+    debug('Found `' + modulePath + '` in `' + base + '` cache');
+    return callback(null, cache[modulePath].path, cache[modulePath].data);
   }
 
   // Find closest package.json to the base.
   if (modulePath === '.') {
-    while (base) {
-      newPath = path.resolve(base, 'package.json');
-      debug('Trying to resolve `' + newPath + '`');
-      if (existsSync(newPath)) {
-        newPath = fs.realpathSync(newPath);
-        cache[modulePath] = newPath;
-        return newPath;
-      }
-      base = base.substr(0, base.lastIndexOf('/'));
-    }
+    find(base, function(err, pkgPath, pkgData) {
+      if (err) return callback(err);
+      cache[modulePath] = {path: pkgPath, data: pkgData};
+      callback(null, pkgPath, pkgData);
+    });
   }
   // Check a relative path for a package.json.
   else if (modulePath[0] === '.' || modulePath[0] === '/') {
-    newPath = path.resolve(base, modulePath, 'package.json');
-    debug('Trying to resolve `' + newPath + '`');
-    if (existsSync(newPath)) {
-      newPath = fs.realpathSync(newPath);
-      cache[modulePath] = newPath;
-      return newPath;
-    }
+    var check = path.resolve(base, modulePath, 'package.json');
+    debug('Trying to resolve `' + check + '`');
+    readJSON(check, function(err, data) {
+      if (err) return callback(err);
+      cache[modulePath] = {path: check, data: data};
+      callback(null, check, data);
+    });
   }
   // Recurse backwards through node_modules looking for the module.
   else {
-    while (base) {
-      newPath = path.resolve(base, 'node_modules', modulePath, 'package.json');
-      debug('Trying to resolve `' + newPath + '`');
-      if (existsSync(newPath)) {
-        newPath = fs.realpathSync(newPath);
-        cache[modulePath] = newPath;
-        return newPath;
-      }
-      base = base.substr(0, base.lastIndexOf('/'));
-    }
+    find(base, modulePath, function(err, pkgPath, pkgData) {
+      if (err) return callback(err);
+      cache[modulePath] = {path: pkgPath, data: pkgData};
+      callback(null, pkgPath, pkgData);
+    });
   }
-
-  // Oops, couldn't find it.
-  var err = new Error("Can't find '" + modulePath + "' relative to '" + originalBase + "'");
-  err.code = 'ENOENT';
-  throw err;
 }
 
 // Returns the directory containing package.json instead of the filename.
-witwip.dir = function(base, modulePath) {
-  return path.dirname(witwip(base, modulePath));
+witwip.dir = function(base, modulePath, callback) {
+  witwip(base, modulePath, function(err, pkgPath, pkgData) {
+    if (err) return callback(err);
+    callback(null, path.dirname(pkgPath), pkgData);
+  });
 };
+
+// Recursively search back up a directory tree for the neareset package.json or
+// a module.
+function find(basePath, modulePath, cb) {
+  var check;
+  if (typeof modulePath === 'function') {
+    cb = modulePath;
+    modulePath = null;
+  }
+  if (!basePath) {
+    var err = new Error("Can't find '" + modulePath + "'");
+    err.code = 'ENOENT';
+    return cb(err);
+  }
+  if (modulePath)
+    check = path.resolve(basePath, 'node_modules', modulePath, 'package.json');
+  else
+    check = path.resolve(basePath, 'package.json');
+
+  debug('Trying to resolve `' + check + '`');
+  readJSON(check, function(err, data) {
+    if (err) {
+      if (err.code && err.code === 'ENOENT') {
+        return find(basePath.substr(0, basePath.lastIndexOf('/')), modulePath, cb);
+      }
+      else {
+        return cb(err);
+      }
+    }
+    else {
+      cb(null, check, data);
+    }
+  });
+}
